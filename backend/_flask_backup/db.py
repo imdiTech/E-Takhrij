ca3f153253f5abc98d8e0ca88ac5e17b464955da
imdie85 <imdipratama@gmail.com>
2026-06-13 14:07:09 +0700

    Remove _flask_backup folder from tracking

--- backend/_flask_backup/db.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = 'admin.login'
-login_manager.login_message = 'Silakan login terlebih dahulu untuk mengakses halaman ini.'
-login_manager.login_message_category = 'warning'
--- backend/_flask_backup/models/__init__.py
+++
@@ -1,2 +0,0 @@
-# Models module
-import models.theme
